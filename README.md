2ce62094d3e60b42ee5f0fc64578249297ab525a
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2021-04-19 22:53:20 -0300

    Saving really cool state, nix + flake works!

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # terraform-ec2
 
-TODO: The `key_name` needs some manual work.
+TODO: The `key_name` (`my-ec2.pem`) needs some manual work.
 
 TODO: $(terraform output public_ip) + ssh
+
+
+```bash
+make destroy \
+&& make apply \
+&& sleep 30 \
+&& ssh ubuntu@$(terraform output public_ip) -i ~/.ssh/my-ec2.pem
+```
--- post_install_scripts/install.sh
+++ post_install_scripts/install.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-#sudo apt install make curl -y
-#curl -fsSL https://get.docker.com -o get-docker.sh
-#sudo sh get-docker.sh
-#sudo curl -L "https://github.com/docker/compose/releases/download/1.26.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-#sudo chmod +x /usr/local/bin/docker-compose
-#rm get-docker.sh
 
-sudo apt-get update
-sudo apt-get install curl -y
+test -d '/nix' || sudo mkdir --mode=0755 '/nix'
+chown 'ubuntu':'ubuntu' '/nix'
 
+export TMPDIR='/tmp'
+
+su ubuntu -c 'curl -fsSL https://raw.githubusercontent.com/ES-Nix/get-nix/288cc322ea5e925d993eb654667ceaa607575a38/get-nix.sh | sh'
+
+chown 'ubuntu':'ubuntu' --recursive '/home/ubuntu'
+
+
+#su ubuntu -c 'env > /home/ubuntu/env.log'
+#su ubuntu -c 'nix --version > /home/ubuntu/out.log'
+#su ubuntu -c 'nix-shell -I nixpkgs=channel:nixos-20.09 --packages nixFlakes --run id > log.log'
+#nix-shell \
+#-I nixpkgs=channel:nixos-20.09 \
+#--packages \
+#nixFlakes \
+#--run \
+#'nix develop github:ES-Nix/nix-flakes-shellHook-writeShellScriptBin-defaultPackage/65e9e5a64e3cc9096c78c452b51cc234aa36c24f --command id'
 
 #file_string=$(echo -e "$(cat <<"EOF"
 #echo test
@@ -25,4 +35,3 @@ sudo apt-get install curl -y
 #)")
 #echo "$file_string" > /home/ubuntu/daily_cron.sh
 #chmod +x /home/ubuntu/daily_cron.sh
-
