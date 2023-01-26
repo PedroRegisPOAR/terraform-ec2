9cdcf374e54f0ec086c68f8f46c2c0c07b855a93
Pedro <pedroalencarregis@hotmail.com>
2023-01-26 15:53:51 -0300

    WIP, tryies public and refactors

--- bucket-nix-cache-test/bucket.tf
+++ bucket-nix-cache-test/bucket.tf
@@ -1,9 +1,37 @@
-module "bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
+resource "aws_s3_bucket" "bucket-nix-cache-test" {
   bucket = local.bucket_name
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = aws_s3_bucket.bucket-nix-cache-test.id
+  policy = <<EOF
+{
+    "Id": "DirectReads",
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowDirectReads",
+            "Action": [
+                "s3:GetObject",
+                "s3:GetBucketLocation"
+            ],
+            "Effect": "Allow",
+            "Resource": [
+                "arn:aws:s3:::example-es",
+                "arn:aws:s3:::example-es/*"
+            ],
+            "Principal": "*"
+        }
+    ]
+}
+EOF
+}
+
+
+resource "aws_s3_bucket_public_access_block" "bucket-nix-cache-test" {
+  bucket = aws_s3_bucket.bucket-nix-cache-test.id
 
   block_public_acls   = true
   block_public_policy = true
-
 }
