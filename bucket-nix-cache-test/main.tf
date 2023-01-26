691a95f38ffac26bac734c204d92fdf4f62610da
Pedro <pedroalencarregis@hotmail.com>
2023-01-26 15:43:59 -0300

    Crates specific folder for bucket creation

---
+++ bucket-nix-cache-test/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+locals {
+  name        = "AWS bucket Terraform"
+  environment = terraform.workspace
+
+  # This is the convention we use to know what belongs to each other
+  resource_name = "${local.name}-${local.environment}"
+  bucket_name = "playing-bucket-nix-cache-test"
+}
