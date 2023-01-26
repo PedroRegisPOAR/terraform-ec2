01199fb83e56b1bd20387d0e7f50cec6f5f506f0
Pedro <pedroalencarregis@hotmail.com>
2023-01-26 03:25:15 -0300

    Uncomments locals

--- main.tf
+++ main.tf
@@ -3,13 +3,13 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-#locals {
-#  name        = "my-ec2"
-#  environment = terraform.workspace
-#
-#  # This is the convention we use to know what belongs to each other
-#  resource_name = "${local.name}-${local.environment}"
-#}
+locals {
+  name        = "my-ec2"
+  environment = terraform.workspace
+
+  # This is the convention we use to know what belongs to each other
+  resource_name = "${local.name}-${local.environment}"
+}
 
 resource "aws_s3_bucket" "example-es" {
   bucket = "example-es"
